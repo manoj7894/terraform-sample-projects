a573cdbf04c13f08f6e03835fead4b4069562600
manojvarma <manojvarma3003@gmail.com>
2023-11-29 12:53:52 +0530

    EC2

--- Ec2/main.tf
+++ Ec2/main.tf
@@ -8,6 +8,8 @@ provider "aws" {
 resource "aws_vpc" "vpc" {
     cidr_block       = "10.0.0.0/16"
     instance_tenancy = "default"
+    #enable_dns_support = true
+    #enable_dns_hostnames = true
     
     tags = {
         Name = "vpc01"
