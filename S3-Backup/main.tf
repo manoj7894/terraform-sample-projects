e9fdc88c4bc2204074fcef73e9eee46363568754
manoj7894 <137042897+manoj7894@users.noreply.github.com>
2024-01-09 17:05:35 +0530

    s3

--- S3-Backup/main.tf
+++ S3-Backup/main.tf
@@ -1,11 +1,11 @@
-# provider details
+# Providers
 provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-# To Create VPC
+# Create an vpc
 resource "aws_vpc" "vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -15,12 +15,11 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-# To Create Public Subnet
+# Create Public-subnet
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.vpc.id # Replace with your VPC ID
-  cidr_block              = "10.0.0.0/24"  # Replace with your desired CIDR block
-  availability_zone       = "ap-south-1a"  # Replace with your desired Availability Zone
-  map_public_ip_on_launch = true           # Enable auto-assign public IP
+  vpc_id            = aws_vpc.vpc.id # Replace with your VPC ID
+  cidr_block        = "10.0.0.0/24"  # Replace with your desired CIDR block
+  availability_zone = "ap-south-1a"  # Replace with your desired Availability Zone
 
   # Optional: Assign tags to your subnets
   tags = {
@@ -28,12 +27,11 @@ resource "aws_subnet" "public" {
   }
 }
 
-# To Create Private Subnet
+# Create Private-subnet
 resource "aws_subnet" "private" {
-  vpc_id                  = aws_vpc.vpc.id # Replace with your VPC ID
-  cidr_block              = "10.0.1.0/24"  # Replace with your desired CIDR block
-  availability_zone       = "ap-south-1b"  # Replace with your desired Availability Zone
-  map_public_ip_on_launch = true           # Enable auto-assign public IP
+  vpc_id            = aws_vpc.vpc.id # Replace with your VPC ID
+  cidr_block        = "10.0.1.0/24"  # Replace with your desired CIDR block
+  availability_zone = "ap-south-1b"  # Replace with your desired Availability Zone
 
   # Optional: Assign tags to your subnets
   tags = {
@@ -41,7 +39,7 @@ resource "aws_subnet" "private" {
   }
 }
 
-# To create the Internet gateway
+# Create Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
@@ -51,7 +49,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# To create Routetable-1
+# create RouteTable-1
 resource "aws_route_table" "rt1" {
   vpc_id = aws_vpc.vpc.id
 
@@ -66,7 +64,7 @@ resource "aws_route_table" "rt1" {
   }
 }
 
-# To create Routetable-2
+# create RouteTable-2
 resource "aws_route_table" "rt2" {
   vpc_id = aws_vpc.vpc.id
 
@@ -81,13 +79,13 @@ resource "aws_route_table" "rt2" {
   }
 }
 
-# To attch Publicsubnet and routetable-1
+# Attach Public Subnet to routetable-1
 resource "aws_route_table_association" "subnet1_association" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.rt1.id
 }
 
-# To attch Privatesubnet and routetable-2
+# Attach Private Subnet to routetable-2
 resource "aws_route_table_association" "subnet2_association" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.rt2.id
@@ -115,7 +113,15 @@ resource "aws_security_group" "example_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # outgoing traffic
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -124,18 +130,29 @@ resource "aws_security_group" "example_security_group" {
   }
 }
 
-# Create an First EC2 instance for master node
+
 resource "aws_instance" "example_instance" {
-  ami                         = var.ami     # Change to your desired AMI ID
-  instance_type               = "t2.medium" # Change to your desired instance type
+  ami                         = var.ami           # Change to your desired AMI ID
+  instance_type               = var.instance_type # Change to your desired instance type
   subnet_id                   = aws_subnet.public.id
   associate_public_ip_address = true         # Enable a public IP
   key_name                    = var.key_name # Change to your key pair name
   availability_zone           = var.availability_zone
-  user_data                   = filebase64("/home/ec2-user/CLI.sh")
+  count                       = var.instance_count
+  user_data                   = filebase64("/home/ec2-user/ec2/file.sh")
   vpc_security_group_ids      = [aws_security_group.example_security_group.id]
+  #user_data = <<-EOF
+  #!/bin/bash
+  #sudo echo "Hello from user data!" > /tmp/file123.txt
+  # You can put any shell script or commands here
+  #EOF
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp2"
+  }
 
   tags = {
-    Name = "EKS-Masternode"
+    Name = "Example-Instance-${count.index}"
   }
 }
